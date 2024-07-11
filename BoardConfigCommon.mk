39cdc0db11841792caf7f732b833bc4f9091c3ec
zaidkhan0997 <kzaid0997@gmail.com>
2024-07-11 13:20:56 +0530

    sm6150-common: Initialize for Evolution X 9.x
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,9 +99,6 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 TARGET_USES_ION := true
 TARGET_DISABLED_UBWC := true
 
-# Partitions
--include vendor/lineage/config/BoardConfigReservedSize.mk
-
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 
 BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
